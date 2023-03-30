1fa14bdf3e556e7d3a3bb2df222a07c0d43cce63
alessandroCassani <acassani@studenti.uninsubria.it>
2023-03-30 14:47:46 +0200

    sistemazione bug double

--- vue_auth/src/App.vue
+++ vue_auth/src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container"></div>
-  <div class="row col-md-12">
+  
     <div>
       <RouterView></RouterView>
     </div>
-  </div>
-<div>
- <RouterView> </RouterView> 
-</div> 
+  
 </template>
 
 <script>
--- vue_auth/src/pages/Log_in.vue
+++ vue_auth/src/pages/Log_in.vue
@@ -1,10 +1,48 @@
 <template>
-   <div>
-      <div>Login Page</div>
-   </div> 
+   <div id="login">
+       <h1>Login</h1>
+       <input type="text" name="username" v-model="input.username" placeholder="Username" />
+       <input type="password" name="password" v-model="input.password" placeholder="Password" />
+       <button type="button" v-on:click="login()">Login</button>
+       <div class="shadow-lg p-3 mb-5 bg-white rounded">Larger shadow</div>
+   </div>
 </template>
 
 <script>
-export default{};
-
+   export default {
+       name: 'Log_in',
+       data() {
+           return {
+               input: {
+                   username: "",
+                   password: ""
+               }
+           }
+       },
+       methods: {
+           login() {
+               if(this.input.username != "" && this.input.password != "") {
+                   if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                       this.$emit("authenticated", true);
+                       this.$router.replace({ name: "secure" });
+                   } else {
+                       console.log("The username and / or password is incorrect");
+                   }
+               } else {
+                   console.log("A username and password must be present");
+               }
+           }
+       }
+   }
 </script>
+
+<style scoped>
+   #login {
+       width: 500px;
+       border: 1px solid #CCCCCC;
+       background-color: #FFFFFF;
+       margin: auto;
+       margin-top: 200px;
+       padding: 20px;
+   }
+</style>
