01d6395d0077ac8d776ad81b78988e23fdccb558
alessandroCassani <acassani@studenti.uninsubria.it>
2023-03-31 11:11:14 +0200

    sistemazione posizione e distanze login

--- vue_auth/src/pages/Log_in.vue
+++ vue_auth/src/pages/Log_in.vue
@@ -1,12 +1,22 @@
 <template>
-   <div id="login">
-       <h1>Login</h1>
-       <input type="text" name="username" v-model="input.username" placeholder="Username" />
-       <input type="password" name="password" v-model="input.password" placeholder="Password" />
-       <button type="button" v-on:click="login()">Login</button>
-       <div class="shadow-lg p-3 mb-5 bg-white rounded">Larger shadow</div>
-   </div>
+<div id="app">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-7 col-sm-8 mx-auto">
+            <h1>Sign In</h1>
+               <form class="form-group">
+                  <input id="inputMail" type="email" class="form-control" placeholder="Email" required>
+                  <input id="inputPsw" type="password" class="form-control" placeholder="Password" required>
+                  <button id="btn" type="button" class="btn btn-outline-success">Success</button>
+                  <p>Don't have an account?>Sign up here</p>
+                  <p>Forgot your password?</p>
+               </form>
+        </div>
+    </div>
+</div>
+</div>
 </template>
+
 
 <script>
    export default {
@@ -37,12 +47,27 @@
 </script>
 
 <style scoped>
-   #login {
-       width: 500px;
-       border: 1px solid #CCCCCC;
-       background-color: #FFFFFF;
-       margin: auto;
-       margin-top: 200px;
-       padding: 20px;
+
+    #app{
+    display: flex;
+    align-items: center;
+    height: 90vh;
+    }
+
+ 
+
+   #inputMail{
+    margin-top: 20px;
    }
+
+   #inputPsw{
+    margin-top: 20px;
+   }
+
+   #btn{
+    margin-top: 20px;
+   }
+
+  
+   
 </style>
