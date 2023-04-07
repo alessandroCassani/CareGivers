47d280eeea082f1624ab4f780a9c3167fcea5801
alessandroCassani <acassani@studenti.uninsubria.it>
2023-04-07 11:34:06 +0200

    connessione mongoDb avvenuta

--- vue_auth/server.js
+++ vue_auth/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { user } = require('./models/user.js')
 const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb+srv://acassani:<Admin123>@caregivers.rgfjqts.mongodb.net/?retryWrites=true&w=majority');
 const app = express();
 const port = process.env.port || 5000;
 
@@ -19,12 +21,25 @@ app.post('/signup',(req, res, next)=> {
         nome: req.body.nome,
         cognome: req.body.cognome,
         codiceFiscale: req.body.codiceFiscale,
-        password: bcrypt.hashSync(req.body.password,10),                                //aggiungere HASH
+        password: bcrypt.hashSync(req.body.password,10),                               
         ruolo: req.body.ruolo
     })
 
+    newUser.save(err=> {
+        if(err){
+            return res.status(400).json({
+                title:'error',
+                error: 'email gia in uso'
+            })       
+        }
+        return res.status(200).json({
+            title: 'registrazione avvenuta con successo'
+        })
+
+
     console.log(newUser);
-})
+})})
+
 
 
 app.listen(port,(err) => {
