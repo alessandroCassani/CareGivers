bd904ce03c884539d050c66bae2d887c23edfd9b
alessandroCassani <acassani@studenti.uninsubria.it>
2023-04-11 11:17:19 +0200

    connessione stabilita db

--- vue_auth/server.js
+++ vue_auth/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = 'mongodb+srv://user:user@caregivers.rgfjqts.mongodb.net/?retryWrites=true&w=majority';
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { user } = require('./models/user.js')
 const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb+srv://acassani:<Admin123>@caregivers.rgfjqts.mongodb.net/?retryWrites=true&w=majority');
+
 const app = express();
 const port = process.env.port || 5000;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const database = async() => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  try {
+    await mongoose.connect('mongodb+srv://user:user@caregivers.rgfjqts.mongodb.net/?retryWrites=true&w=majority')
+    console.log('DB connected')
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,7 +32,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 //routes
 app.post('/signup',(req, res, next)=> {
-    console.log(req.body.nome);
     const newUser = new user({
         email:req.body.email,
         nome: req.body.nome,
@@ -25,20 +41,23 @@ app.post('/signup',(req, res, next)=> {
         ruolo: req.body.ruolo
     })
 
-    newUser.save(err=> {
-        if(err){
-            return res.status(400).json({
-                title:'error',
-                error: 'email gia in uso'
-            })       
-        }
-        return res.status(200).json({
-            title: 'registrazione avvenuta con successo'
-        })
+    console.log('ciao');
+    
+
+  //  newUser.save(err=> {
+    //    if(err){
+      //      return res.status(400).json({
+        //        title:'error',
+          //      error: 'email gia in uso'
+            //})       
+       // }
+        //return res.status(200).json({
+          //  title: 'registrazione avvenuta con successo'
+        //})
 
 
-    console.log(newUser);
-})})
+   // console.log(newUser);
+})
 
 
 
