4ee1f9c17ec01225094bc19f4e480d6afdef1675
alessandroCassani <acassani@studenti.uninsubria.it>
2023-04-11 16:06:50 +0200

    modifica schema user

--- vue_auth/models/user.js
+++ vue_auth/models/user.js
@@ -4,16 +4,31 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const userSchema = new schema({
     nome: String,
-    email: {
-        unique : true,
-        type: String
-    },
-    password: String,
     cognome: String,
     dataDiNascita: Date,
     codiceFiscale: String,
-    ruolo: String
+    password: String,
+    confermaPassword: String,
+    ruolo: String,
+    recapiti: {
+        email: String,
+        telefono1: String,
+        telefono2: String
+    },
+    patologie: [{
+        patologia: String
+    }],
+    terapie:[{
+        farmaco: String,
+        dosaggio: Number
+    }]
 });
 
 const user = mongoose.model('users',userSchema);
 module.exports = { user };
+
+
+email: {
+    unique : true,
+    type: String
+},
