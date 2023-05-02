083cceb23c6f2827516711c3ffad5a141e2ae195
alessandroCassani <acassani@studenti.uninsubria.it>
2023-05-02 11:15:02 +0200

    eliminazione pt 2

--- vue_auth/src/routes.js
+++ vue_auth/src/routes.js
@@ -3,11 +3,13 @@ import {createRouter,createWebHistory} from 'vue-router';
 import Log_in from './components/Log_in.vue';
 import Sign_Up from './components/Sign_up.vue';
 import referenti from './components/referenti.vue';
+import memo from './components/Memo.vue'
 
 const routes = [{
     path: '/login', component: Log_in, name:"login",},
     {path: '/signup', component: Sign_Up, name:"signUp"},
-    {path:'/referenti', component: referenti, name:'referenti'}
+    {path:'/referenti', component: referenti, name:'referenti'},
+    {path: '/memo', component: memo, name: '/memo'}
 ];
 
 const router = createRouter({
