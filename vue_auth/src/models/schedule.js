544e22463edbf5d3af34be0d7498410e7844a547
alessandroCassani <acassani@studenti.uninsubria.it>
2023-05-08 15:23:20 +0200

    modifica model schedule e metodo .pre

--- vue_auth/src/models/schedule.js
+++ vue_auth/src/models/schedule.js
@@ -3,8 +3,16 @@ const mongoose = require('mongoose');
 const memoSchema = new mongoose.Schema({
   evento: String,
   data: String,
-  orario: String,
-  expiresAt: Date,
+  expirationTime: { type: Number, required: true },
+  expireAt: { type: Date, default: Date.now, index: { expires: 0 } }
+});
+
+memoSchema.pre('save', function() {
+  // Calculate the expiration time based on the user-selected value
+  const expirationTimeInSeconds = this.expirationTime * 60;
+  
+  // Set the `expires` option for the TTL index
+  this.index({ expireAt: 1 }, { expireAfterSeconds: expirationTimeInSeconds });
 });
 
 const Memo = mongoose.model('Memo', memoSchema);
