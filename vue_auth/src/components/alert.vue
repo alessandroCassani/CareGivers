3a5979795f9c3c131e4259b4d231f887d174d585
alessandroCassani <acassani@studenti.uninsubria.it>
2023-05-14 22:06:42 +0200

    creazione pagina alert

---
+++ vue_auth/src/components/alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <Side_bar> </Side_bar>
+</template>
+
+<script>
+import Side_bar from "@/components/Side_bar.vue";
+import sidebarWidth from "@/components/state";
+
+export default {
+  name: "alert",
+  components: { Side_bar },
+
+  data() {
+    return {
+      data: "",
+      sidebarWidth,
+    };
+  },
+};
+</script>
+
+<style></style>
