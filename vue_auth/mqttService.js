f7e11d7c21db9e07c189021bc1303bb7633acce9
alessandroCassani <acassani@studenti.uninsubria.it>
2023-05-18 11:08:10 +0200

    installazione mosca e jsonschema con broker runnin

--- vue_auth/mqttService.js
+++ vue_auth/mqttService.js
@@ -1,38 +1,9 @@
-const mqtt = require('mqtt')
+const mosca = require('mosca')
+var settings = { port: 1234}
+var broker =  new mosca.Server(settings)
 
-const host = 'broker.emqx.io'
-const port = '1883'
-const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+broker.on('ready', ()=>{
+  console.log('broker is running')
 
-const connectUrl = `mqtt://${host}:${port}`
-
-const client = mqtt.connect(connectUrl, {
-  clientId,
-  clean: true,
-  connectTimeout: 4000,
-  username: 'emqx',
-  password: 'public',
-  reconnectPeriod: 1000,
 })
 
-const topic = '/nodejs/mqtt'
-
-client.on('connect', () => {
-  console.log('Connected')
-  client.subscribe([topic], () => {
-    console.log(`Subscribe to topic '${topic}'`)
-  })
-})
-
-
-client.on('message', (topic, payload) => {
-  console.log('Received Message:', topic, payload.toString())
-})
-
-client.on('connect', () => {
-  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-    if (error) {
-      console.error(error)
-    }
-  })
-})
