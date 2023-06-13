609426f96809bd102c427a5eea3adad9ea223ac8
alessandroCassani <acassani@studenti.uninsubria.it>
2023-06-13 12:11:46 +0200

    form to from

--- vue_auth/src/main.js
+++ vue_auth/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes';
-import mqttModule form './mqtt'
+import mqttModule from './mqtt'
 
 
 const app = createApp(App);
