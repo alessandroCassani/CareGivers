4ae850581aed9927a2d1f8b07d6c08de700d8ad3
alessandroCassani <acassani@studenti.uninsubria.it>
2023-07-04 17:41:39 +0200

    modifica key

--- vue_auth/frontend/src/components/cipher.js
+++ vue_auth/frontend/src/components/cipher.js
@@ -1,15 +1,14 @@
 const crypto = require('crypto');
 
 export function encrypt(plaintext) {
-  console.log(process.env.VUE_APP_SECRET_KEY)
-  const cipher = crypto.createCipher('aes-256-cbc', 'ciao');
+  const cipher = crypto.createCipher('aes-256-cbc', 'mwkldnwlkcndwjncvkj');
   let encrypted = cipher.update(plaintext, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return encrypted;
 }
 
 export function decrypt(ciphertext) {
-  const decipher = crypto.createDecipher('aes-256-cbc', 'ciao');
+  const decipher = crypto.createDecipher('aes-256-cbc', 'mwkldnwlkcndwjncvkj');
   let decrypted = decipher.update(ciphertext, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
   return decrypted;
