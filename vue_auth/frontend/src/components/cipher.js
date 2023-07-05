7e2a24048b6cd6c63485cbb59be11cc017e02127
alessandroCassani <acassani@studenti.uninsubria.it>
2023-07-05 11:24:29 +0200

    eliminazione node modules

--- vue_auth/frontend/src/components/cipher.js
+++ vue_auth/frontend/src/components/cipher.js
@@ -1,14 +1,14 @@
 const crypto = require('crypto');
 
 export function encrypt(plaintext) {
-  const cipher = crypto.createCipher('aes-256-cbc', 'mwkldnwlkcndwjncvkj');
+  const cipher = crypto.createCipher('aes-256-cbc', 'ciao');
   let encrypted = cipher.update(plaintext, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return encrypted;
 }
 
 export function decrypt(ciphertext) {
-  const decipher = crypto.createDecipher('aes-256-cbc', 'mwkldnwlkcndwjncvkj');
+  const decipher = crypto.createDecipher('aes-256-cbc', 'ciao');
   let decrypted = decipher.update(ciphertext, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
   return decrypted;
